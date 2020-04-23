72b3394944af269c178cb562e68904f68acfc5d9
Eelco Wiersma <contact@pagebakers.nl>
2020-04-23 09:10:15 +0200

    Update install notice

--- templates/notice.php
+++ templates/notice.php
@@ -1,3 +1,3 @@
 <div class="update-nag belco-install-notice">
-	<?php _e( 'Connect your Belco account to start receiving calls.', 'wp_belco' ); ?> <a href="<?php echo admin_url('admin.php?page=belco');?>">Connect now</a>.
+	<?php _e( 'Connect your Belco account to enable the chat widget and sync customer data.', 'wp_belco' ); ?> <a href="<?php echo admin_url('admin.php?page=belco');?>">Connect now</a>.
 </div>
